b5f6bb6842d214ad5cc1563b0620f3a82c1ede7a
Lucy Lima <lucyatmosphere@gmail.com>
2025-05-23 23:38:09 -0300

    feat: question 8 b) solved

--- questao8.c
+++ questao8.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // questao a
     float somatorio = 0.0;
     float contador = 1.0;
 
@@ -10,6 +11,20 @@ int main()
         somatorio = somatorio + (1.0 / contador);
         contador++;
         contador++;
+        printf("%f -%f \n", somatorio, contador);
+    }
+    printf("----------------------------------\n");
+
+    // questao b
+    somatorio = 0.0;
+    contador = 1.0;
+
+    while (contador <= 40)
+    {
+        somatorio = somatorio - (1.0 / contador);
+        contador++;
+        contador++;
+        somatorio = somatorio + (1.0 / contador);
         printf("%f -%f \n", somatorio, contador);
     }
 
