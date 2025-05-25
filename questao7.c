780558fa0aac19f58af42ac79fe92921912ea22d
Lucy Lima <lucyatmosphere@gmail.com>
2025-05-25 19:50:09 -0300

    feat: question 7 solved

---
+++ questao7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int perfeito = 0;
+    int entrada = 0;
+    int divisor = 0;
+    int contador = 1;
+    
+    printf("digite um numero e veja se ele e perfeito ou nao >> ");
+    scanf("%d", entrada);
+
+    while (contador < entrada)
+    {
+        divisor = entrada % contador;
+        if (divisor == 0)
+        {
+            perfeito = perfeito + contador;
+            printf("%d - ", perfeito);
+            printf("%d\n", contador);
+        }
+        contador++;
+    }
+
+    if (perfeito == entrada)
+    {
+        printf("numero e perfeito");
+    }
+    else
+    {
+        printf("numero nao e perfeito");
+    }
+
+    return 0;
+}
