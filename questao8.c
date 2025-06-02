fd5349a12af51d9f390ebb38134a680131d4aa17
Lucy Lima <lucyatmosphere@gmail.com>
2025-06-02 16:43:32 -0300

    fix: answer was incorrect

--- questao8.c
+++ questao8.c
@@ -19,13 +19,19 @@ int main()
     somatorio = 0.0;
     contador = 1.0;
 
-    while (contador <= 40)
+    int i = 0;
+    while (i < 20)
     {
+        if (i % 2 == 0)
+        {
+            somatorio = somatorio + (1.0 / contador);
+        }
         somatorio = somatorio - (1.0 / contador);
         contador++;
         contador++;
         somatorio = somatorio + (1.0 / contador);
-        printf("%f -%f \n", somatorio, contador);
+        printf("%f - %f \n", somatorio, contador);
+        i++;
     }
 
     return 0;
